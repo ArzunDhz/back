fac90a1ce880c600551b3fe73c42d830efddcb43
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2023-07-12 23:49:13 +0545

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,11 +1,11 @@
 
-
+import { app } from "../app"
 
 export const errorMiddleWare = ()=>{
     app.use((err,req,res,next)=>{
         return res.status(404).send({
           success:false,
-          message:err.message+ 'dasda'
+          message:err.message
          })  
      })
 
