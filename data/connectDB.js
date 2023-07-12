95395b29e76dfd219697e430b5247791e165a749
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2023-07-12 23:23:45 +0545

    Update connectDB.js

--- data/connectDB.js
+++ data/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDataBase = ()=>{
 mongoose
-  .connect("mongodb://127.0.0.1:27017", {
+  .connect("mongodb+srv://admin-arjun:9869579607Aa1@cluster0.ldmnk.mongodb.net/?retryWrites=true", {
     dbName: "TodoBackend",
   })
   .then(() => console.log("database connected"));
